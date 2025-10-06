c906f1c9155c61066c0fd4129a45197c4265671a
codewith-isha <codeoncanvas20@gmail.com>
2025-10-07 00:43:33 +0530

    env

--- src/utils/rapid.js
+++ src/utils/rapid.js
@@ -1,10 +1,20 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
+const BASE_URL = "https://youtube138.p.rapidapi.com"
 const options = {
 	method: 'GET',
-	hostname: 'youtube138.p.rapidapi.com',
-	port: null,
-	path: '/auto-complete/?q=desp&hl=en&gl=US',
 	headers: {
-		'x-rapidapi-key': 'b84639bc90msh4ee152098dc0442p14b939jsn024df7769e29',
+		'x-rapidapi-key': API_KEY,
 		'x-rapidapi-host': 'youtube138.p.rapidapi.com'
 	}
 };
+
+export const fetchData = async(url)=>{
+  try{
+    const {data} = await axios.get(`${BASE_URL}/${url}` , options)
+    return data
+  }catch(error){
+    console.error('error fetching api data:' , error)
+  }
+}
